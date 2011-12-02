8503543723e22137f3748a403c2c1d2e47e39978
Sergej <eipi1is0@googlemail.com>
2011-12-02 11:31:03 +0100

    graph als template nur noch in der header. makefile tut.

--- source/pferd.cpp
+++ source/pferd.cpp
@@ -2,11 +2,12 @@
 
 int main(){
 	
-	/*
+	///*
+	// Test des Graphen: instanziieren und alles mal ausprobieren 
 	Graph<Edge, Node, Shortcut> g = Graph<Edge, Node, Shortcut>();
 	g.initOffsets();
 	g.clearShortcuts();
-	*/
+	// */
 
 	return 0;
 }
