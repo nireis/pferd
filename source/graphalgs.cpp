2247c049ce414251f65428bbf8e25563a3869997
Chaot <git.andre.nusser@googlemail.com>
2011-12-14 13:21:47 +0100

    Bidijkstra angefangen

--- source/graphalgs.cpp
+++ source/graphalgs.cpp
@@ -37,16 +37,16 @@ void Dijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id){
 	for(int i=0; i<10; i++){
 		// Iterator für die ausgehenden Kanten eines Knotens
 		Graph<Edge, Node, Shortcut>::OutEdgesIterator it = g->getOutEdgesIt(node_id);
-		// Pointer um die akutelle Kante zu behandeln
-		Edge* nextEdge;
 		// Die priotiry_queue, welche der Menge U im Dijkstra entspricht
 		std::priority_queue<U_element, std::vector<U_element>, Compare_U_element> U;
 
 		unsigned int nr_of_nodes = g->getNodeCount();
-		// Die ganzen Vektoren initialisieren
 		vector<unsigned int> dist(nr_of_nodes,numeric_limits<unsigned int>::max());
-		vector<bool> found(nr_of_nodes,false);
 		vector<unsigned int> in_edge_id(nr_of_nodes);
+
+		Edge* currentEdge;
+
+		vector<bool> found(nr_of_nodes,false);
 
 		//TEST
 		clock_t start,finish;
@@ -56,9 +56,9 @@ void Dijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id){
 		dist[node_id] = 0;
 		found[node_id] = true;
 		while(it.hasNext()){
-			nextEdge = it.getNext();
+			currentEdge = it.getNext();
 			// Die Knoten mit ihrer Distanz in U stecken
-			U.push(U_element(nextEdge->distance,nextEdge->target,nextEdge->id));
+			U.push(U_element(currentEdge->distance,currentEdge->target,currentEdge->id));
 		}
 
 		// Die restlichen Knoten abarbeiten
@@ -73,12 +73,12 @@ void Dijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id){
 				// Die ausgehenden Kanten durchgehen und in U werfen
 				it = g->getOutEdgesIt(tmpid);
 				while(it.hasNext()){
-					nextEdge = it.getNext();
+					currentEdge = it.getNext();
 					// Wenn sie noch nicht gefunden wurde...
-					if(!found[nextEdge->target]){
+					if(!found[currentEdge->target]){
 						// ...tu sie in U
 						U.push(U_element(
-									nextEdge->distance+dist[tmpid],nextEdge->target,nextEdge->id));
+									currentEdge->distance+dist[tmpid],currentEdge->target,currentEdge->id));
 					}
 				}
 			}
@@ -127,7 +127,7 @@ void Dijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id){
 	// TEST mit direkt auf Graphstrukturen arbeiten
 	for(int i=0; i<10; i++){
 		// Pointer um die akutelle Kante zu behandeln
-		Edge* nextEdge;
+		Edge* currentEdge;
 		// Die priotiry_queue, welche der Menge U im Dijkstra entspricht
 		std::priority_queue<U_element, std::vector<U_element>, Compare_U_element> U;
 
@@ -145,9 +145,9 @@ void Dijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id){
 		dist[node_id] = 0;
 		found[node_id] = true;
 		for(unsigned int i=g->getLowerEdgeBound(node_id); i<g->getUpperEdgeBound(node_id); i++){
-			nextEdge = g->getEdge(i);
+			currentEdge = g->getEdge(i);
 			// Die Knoten mit ihrer Distanz in U stecken
-			U.push(U_element(nextEdge->distance,nextEdge->target,nextEdge->id));
+			U.push(U_element(currentEdge->distance,currentEdge->target,currentEdge->id));
 		}
 
 		// Die restlichen Knoten abarbeiten
@@ -161,12 +161,12 @@ void Dijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id){
 				in_edge_id[tmpid] = U.top().eid;
 				// Die ausgehenden Kanten durchgehen und in U werfen
 				for(unsigned int i=g->getLowerEdgeBound(tmpid); i<g->getUpperEdgeBound(tmpid); i++){
-					nextEdge = g->getEdge(i);
+					currentEdge = g->getEdge(i);
 					// Wenn sie noch nicht gefunden wurde...
-					if(!found[nextEdge->target]){
+					if(!found[currentEdge->target]){
 						// ...tu sie in U
 						U.push(U_element(
-									nextEdge->distance+dist[tmpid],nextEdge->target,nextEdge->id));
+									currentEdge->distance+dist[tmpid],currentEdge->target,currentEdge->id));
 					}
 				}
 			}
@@ -211,5 +211,62 @@ void Dijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id){
 	}
 }
 
-void BiDijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id){
+void BiDijkstra(Graph<Edge, Node, Shortcut>* g, unsigned int node_id1, unsigned int node_id2){
+	// Iterator für die ausgehenden und eingehenden Kanten eines Knotens
+	Graph<Edge, Node, Shortcut>::OutEdgesIterator itout = g->getOutEdgesIt(node_id1);
+	Graph<Edge, Node, Shortcut>::InEdgesIterator itin = g->getInEdgesIt(node_id2);
+
+	// Die priotiry_queues, welche der Menge U im Dijkstra entsprechen
+	std::priority_queue<U_element, std::vector<U_element>, Compare_U_element> U1;
+	std::priority_queue<U_element, std::vector<U_element>, Compare_U_element> U2;
+
+	unsigned int nr_of_nodes = g->getNodeCount();
+	unsigned int current_min = numeric_limits<unsigned int>::max();
+	vector<unsigned int> dist(nr_of_nodes,numeric_limits<unsigned int>::max());
+	vector<unsigned int> dijk_edge_id(nr_of_nodes);
+
+	Edge* currentEdge;
+
+	vector<bool> found1(nr_of_nodes,false);
+	vector<bool> found2(nr_of_nodes,false);
+
+	//TEST
+	clock_t start,finish;
+	start = clock();
+
+	// Den ersten Knoten abarbeiten
+	/*dist[node_id] = 0;
+	found[node_id] = true;
+	while(it.hasNext()){
+		currentEdge = it.getNext();
+		// Die Knoten mit ihrer Distanz in U stecken
+		U.push(U_element(currentEdge->distance,currentEdge->target,currentEdge->id));
+	}
+
+	// Die restlichen Knoten abarbeiten
+	unsigned int tmpid;
+	while(!U.empty()){
+		// Die Distanz Eintragen, wenn der kürzeste gefunden wurde (und weiter suchen)
+		tmpid = U.top().id;
+		if(!found[tmpid]){
+			dist[tmpid] = U.top().distance;
+			found[tmpid] = true;
+			in_edge_id[tmpid] = U.top().eid;
+			// Die ausgehenden Kanten durchgehen und in U werfen
+			it = g->getOutEdgesIt(tmpid);
+			while(it.hasNext()){
+				currentEdge = it.getNext();
+				// Wenn sie noch nicht gefunden wurde...
+				if(!found[currentEdge->target]){
+					// ...tu sie in U
+					U.push(U_element(
+								currentEdge->distance+dist[tmpid],currentEdge->target,currentEdge->id));
+				}
+			}
+		}
+		U.pop();
+	}*/
+	// TEST
+	finish = clock();
+	cout << "Zeit für Dijkstra in Sek: " << (double(finish)-double(start))/CLOCKS_PER_SEC << endl;
 }
