8652d5aa8cf8c592efe95ab0df00ce086157c7ec
invor <Invor@pegwellbay.(none)>
2012-03-20 15:50:38 +0100

    added updated node/edge shaders

--- src/pferdVertex.glsl
+++ src/pferdVertex.glsl
@@ -4,14 +4,14 @@ uniform mat4 projection_matrix;
 uniform mat4 model_matrix;
 uniform mat4 view_matrix;
 
-in vec2 in_position;
-in float in_color;
+attribute vec2 in_position;
+attribute float in_color;
 
-out float color;
+varying float color;
 
 
 void main(void) 
 {
 	color = in_color;
-	gl_Position = projection_matrix * view_matrix * model_matrix * vec4(in_position,0.0, 1.0);
+	gl_Position = projection_matrix * view_matrix * model_matrix * vec4(in_position.x*0.6649,in_position.y,0.0, 1.0);
 }
